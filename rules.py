ba21393320e516ac1cad518b19a8909522ee0f4d
Dmitry Bubyagin <dmitry.bubyagin@gmail.com>
2026-06-06 12:41:41 +0100

    Fix reversed road culling bug, add missing is_valid_highway helper, and whitelist service/living_street/pedestrian roads

--- rules.py
+++ rules.py
@@ -1,14 +1,23 @@
 # rules.py
 
-# Whitelisted road categories (ordered from minor to major)
-ROAD_CATEGORIES = ['residential', 'unclassified', 'tertiary', 'secondary', 'primary', 'trunk', 'motorway']
+# Whitelisted road categories (ordered from major to minor)
+ROAD_CATEGORIES = [
+    'motorway', 'trunk', 'primary', 'secondary', 'tertiary', 
+    'unclassified', 'residential', 'living_street', 'service', 'pedestrian'
+]
 
 # All valid highways tags (including link variations)
 VALID_HIGHWAYS = {
     'motorway', 'trunk', 'primary', 'secondary', 'tertiary',
-    'unclassified', 'residential',
+    'unclassified', 'residential', 'living_street', 'service', 'pedestrian',
     'motorway_link', 'trunk_link', 'primary_link', 'secondary_link', 'tertiary_link'
 }
+
+def is_valid_highway(sub_type):
+    """
+    Checks if a highway sub-type is whitelisted for rendering.
+    """
+    return sub_type in VALID_HIGHWAYS
 
 def get_parent_road_type(sub_type):
     """
@@ -38,17 +47,35 @@ def get_road_width_for_scale(road_type, scale, is_interacting, lod_roads_thresho
         
     # Dynamic width sizing mapped to zoom bands
     if scale < 0.00015:
-        base_widths = {'motorway': 1.5, 'trunk': 1.0, 'primary': 0.0, 'secondary': 0.0, 'tertiary': 0.0, 'unclassified': 0.0, 'residential': 0.0}
+        base_widths = {
+            'motorway': 1.5, 'trunk': 1.0, 'primary': 0.0, 'secondary': 0.0, 'tertiary': 0.0, 
+            'unclassified': 0.0, 'residential': 0.0, 'living_street': 0.0, 'service': 0.0, 'pedestrian': 0.0
+        }
     elif scale < 0.0004:
-        base_widths = {'motorway': 2.0, 'trunk': 1.5, 'primary': 1.0, 'secondary': 0.0, 'tertiary': 0.0, 'unclassified': 0.0, 'residential': 0.0}
+        base_widths = {
+            'motorway': 2.0, 'trunk': 1.5, 'primary': 1.0, 'secondary': 0.0, 'tertiary': 0.0, 
+            'unclassified': 0.0, 'residential': 0.0, 'living_street': 0.0, 'service': 0.0, 'pedestrian': 0.0
+        }
     elif scale < 0.0012:
-        base_widths = {'motorway': 2.5, 'trunk': 2.0, 'primary': 1.5, 'secondary': 1.0, 'tertiary': 0.8, 'unclassified': 0.0, 'residential': 0.0}
+        base_widths = {
+            'motorway': 2.5, 'trunk': 2.0, 'primary': 1.5, 'secondary': 1.0, 'tertiary': 0.8, 
+            'unclassified': 0.0, 'residential': 0.0, 'living_street': 0.0, 'service': 0.0, 'pedestrian': 0.0
+        }
     elif scale < 0.004:
-        base_widths = {'motorway': 3.5, 'trunk': 3.0, 'primary': 2.5, 'secondary': 1.8, 'tertiary': 1.2, 'unclassified': 1.0, 'residential': 0.0}
+        base_widths = {
+            'motorway': 3.5, 'trunk': 3.0, 'primary': 2.5, 'secondary': 1.8, 'tertiary': 1.2, 
+            'unclassified': 1.0, 'residential': 0.0, 'living_street': 0.0, 'service': 0.0, 'pedestrian': 0.0
+        }
     elif scale < 0.015:
-        base_widths = {'motorway': 5.0, 'trunk': 4.5, 'primary': 3.5, 'secondary': 2.5, 'tertiary': 2.0, 'unclassified': 1.5, 'residential': 1.0}
+        base_widths = {
+            'motorway': 5.0, 'trunk': 4.5, 'primary': 3.5, 'secondary': 2.5, 'tertiary': 2.0, 
+            'unclassified': 1.5, 'residential': 1.0, 'living_street': 0.8, 'service': 0.6, 'pedestrian': 0.6
+        }
     else:
-        base_widths = {'motorway': 7.0, 'trunk': 6.0, 'primary': 5.0, 'secondary': 4.0, 'tertiary': 3.0, 'unclassified': 2.5, 'residential': 1.5}
+        base_widths = {
+            'motorway': 7.0, 'trunk': 6.0, 'primary': 5.0, 'secondary': 4.0, 'tertiary': 3.0, 
+            'unclassified': 2.5, 'residential': 1.5, 'living_street': 1.2, 'service': 1.0, 'pedestrian': 1.0
+        }
         
     return base_widths.get(road_type, 1.0)
 
